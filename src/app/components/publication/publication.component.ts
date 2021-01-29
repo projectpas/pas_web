44e4565ea8dad003bc547c92dfc7f2ae41705114
Hemant Patel <hemant_patel1234@yahoo.co.in>
2021-01-29 16:01:30 +0530

    Item Master Capes Issue fix.

--- src/app/components/publication/publication.component.ts
+++ src/app/components/publication/publication.component.ts
@@ -242,7 +242,7 @@ export class PublicationComponent implements OnInit, AfterViewInit {
             { field: 'publicationType', header: 'Pub Type' },
             { field: 'publishedBy', header: 'Published By' },
             { field: 'location', header: 'Location' },
-            { field: 'verifiedBy', header: 'Verified By' },
+            { field: 'verifiedByName', header: 'Verified By' },
             { field: 'verifiedDate', header: 'Verified Date' },
         ];
         this.breadCrumb.currentUrl = '/singlepages/singlepages/app-publication';
