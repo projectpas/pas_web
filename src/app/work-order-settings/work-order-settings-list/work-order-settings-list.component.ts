76da86b0c61592ee567217a99d377af9c2ae34d1
mahesh@silverxis.com <mahesh@silverxis.com>
2021-02-03 21:34:32 -0800

    Work order Changes

--- src/app/work-order-settings/work-order-settings-list/work-order-settings-list.component.ts
+++ src/app/work-order-settings/work-order-settings-list/work-order-settings-list.component.ts
@@ -1,21 +1,13 @@
 ﻿import { Component, Input, } from '@angular/core';
-
 declare var $ : any;
-
-import { AlertService, MessageSeverity } from '../../services/alert.service';
-import { ItemMasterService } from '../../services/itemMaster.service';
+import { AlertService} from '../../services/alert.service';
 import { MatTableDataSource } from '@angular/material';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap/modal/modal-ref';
-
 import { AuditHistory } from '../../models/audithistory.model';
 import { AuthService } from '../../services/auth.service';
-import { CustomerService } from '../../services/customer.service';
-import { ConditionService } from '../../services/condition.service';
-import { VendorService } from '../../services/vendor.service';
-import { SiteService } from '../../services/site.service';
 import { Router, ActivatedRoute } from '@angular/router';
-
+import { MenuItem } from 'primeng/api';//bread crumb
 import { WorkOrderService } from '../../services/work-order/work-order.service';
 import { WorkOrderSettingsService } from '../../services/work-order-settings.service';
 
@@ -26,13 +18,6 @@ import { WorkOrderSettingsService } from '../../services/work-order-settings.ser
 })
 /** work-order-settings-list component*/
 export class WorkOrderSettingsListComponent {
-    /** work-order-settings-list ctor */
-    //constructor() {
-
-    //}
-
-    //testing data
-
     @Input() isWorkOrder;
     @Input() workFlowId;
     @Input() workFlowType;
@@ -75,6 +60,7 @@ export class WorkOrderSettingsListComponent {
     allVendors: any[];
     responseDataForWorkFlow: Object;
     noDatavailable: any;
+    breadcrumbs: MenuItem[];
     constructor(private workOrderService: WorkOrderService,
         private route: Router,
         private receivingCustomerWorkOrderService: WorkOrderSettingsService,
@@ -93,6 +79,11 @@ export class WorkOrderSettingsListComponent {
         this.getAllWorkflows();
       
 
+            this.breadcrumbs = [
+                { label: 'Work Order Settings' },
+                { label: ' Work Order Settings List' },
+            ];
+        
     }
 
     public allWorkFlows: any[] = [];
@@ -204,106 +195,6 @@ export class WorkOrderSettingsListComponent {
             task.selected = false;
         }
     }
-
     publicationTypes: any[];
-
-    
-    //private setPublicationData(selectedPublication: any, row: any) {
-    //    if (selectedPublication != null) {
-    //        row.publicationName = selectedPublication.publicationId;
-    //        row.publicationDescription = selectedPublication.description != null ? selectedPublication.description : '';
-    //        row.revisionDate = selectedPublication.revisionDate != null ? new Date(selectedPublication.revisionDate).toLocaleDateString() : '';
-    //        row.publicationType = selectedPublication.publicationTypeId != null ? selectedPublication.publicationTypeId : '';
-    //        row.sequence = selectedPublication.sequence != null ? selectedPublication.sequence : '';
-    //        row.itemMasterAircraftMapping = selectedPublication.itemMasterAircraftMapping;
-    //        row.source = selectedPublication.asd != null ? selectedPublication.asd : '';
-    //        row.location = selectedPublication.location != null ? selectedPublication.location : '';
-    //        row.verifiedBy = selectedPublication.verifiedBy != null ? selectedPublication.verifiedBy : '';
-    //        row.status = selectedPublication.isActive != null ? selectedPublication.isActive : '';
-    //        row.verifiedDate = selectedPublication.verifiedDate != undefined ? new Date(selectedPublication.verifiedDate).toLocaleDateString() : '';
-    //        row.attachmentURL = selectedPublication.attachmentDetails != null ? selectedPublication.attachmentDetails.link : '';
-    //    }
-    //    else {
-    //        row.publicationDescription = '';
-    //        row.revisionDate = '';
-    //        row.publicationType = 0;
-    //        row.sequence = '';
-    //        row.source = '';
-    //        row.location = '';
-    //        row.verifiedBy = '';
-    //        row.status = 0;
-    //        row.verifiedDate = '';
-    //        row.attachmentURL = '';
-    //    }
-    //}
-
-    //private loadcustomerData() {
-    //    this.customerService.getWorkFlows().subscribe(data => {
-    //        this.allCustomers = data[0];
-    //        var selectectedCustomer = this.allCustomers.filter(x => x.customerId == this.sourceWorkFlow.customerId);
-    //        if (selectectedCustomer.length > 0) {
-    //            this.sourceWorkFlow.customerName = selectectedCustomer[0].name;
-    //        }
-
-    //    });
-    //}
-
-    //private calculatePercentOfNew(myValue, percentValue) {
-    //    this.sourceWorkFlow.percentOfNew = "";
-    //    if (myValue && percentValue) {
-    //        this.sourceWorkFlow.percentOfNew = (myValue / 100) * percentValue;
-    //    }
-    //    this.berDetermination();
-    //}
-
-    //private calculatePercentOfReplacement(myValue, percentValue) {
-    //    this.sourceWorkFlow.percentOfReplacement = "";
-    //    if (myValue && percentValue) {
-    //        let afterpercent = myValue / 100;
-    //        this.sourceWorkFlow.percentOfReplacement = afterpercent * percentValue;
-
-    //    }
-    //    this.berDetermination();
-    //}
-
-    //private berDetermination(): any {
-    //    if (this.sourceWorkFlow.fixedAmount != undefined && this.sourceWorkFlow.fixedAmount != "") {
-    //        this.sourceWorkFlow.berThresholdAmount = this.sourceWorkFlow.fixedAmount;
-    //    }
-    //    // check on is percentOfNew enable
-    //    if (this.sourceWorkFlow.percentOfNew != undefined && this.sourceWorkFlow.percentOfNew != "") {
-    //        this.sourceWorkFlow.berThresholdAmount = this.sourceWorkFlow.percentOfNew;
-    //    }
-    //    // check on is .percentOfReplacement enable
-    //    if (this.sourceWorkFlow.percentOfReplacement != undefined && this.sourceWorkFlow.percentOfReplacement != "") {
-    //        this.sourceWorkFlow.berThresholdAmount = this.sourceWorkFlow.percentOfReplacement;
-    //    }
-
-
-    //    // 1 and 2 check box 
-    //    if (this.sourceWorkFlow.fixedAmount != undefined && this.sourceWorkFlow.fixedAmount != "" && this.sourceWorkFlow.percentOfNew != undefined && this.sourceWorkFlow.percentOfNew != "") {
-    //        this.sourceWorkFlow.berThresholdAmount = Math.min(this.sourceWorkFlow.fixedAmount, this.sourceWorkFlow.percentOfNew);
-    //    }
-
-    //    // 2 and 3  check box 
-    //    if (this.sourceWorkFlow.percentOfNew != undefined && this.sourceWorkFlow.percentOfNew != "" && this.sourceWorkFlow.percentOfReplacement != undefined && this.sourceWorkFlow.percentOfReplacement != "") {
-    //        this.sourceWorkFlow.berThresholdAmount = Math.min(this.sourceWorkFlow.percentOfNew, this.sourceWorkFlow.percentOfReplacement);
-    //    }
-    //    // 1 and 3  check box 
-    //    if (this.sourceWorkFlow.fixedAmount != undefined && this.sourceWorkFlow.fixedAmount != "" && this.sourceWorkFlow.percentOfReplacement != undefined && this.sourceWorkFlow.percentOfReplacement != "") {
-    //        this.sourceWorkFlow.berThresholdAmount = Math.min(this.sourceWorkFlow.fixedAmount, this.sourceWorkFlow.percentOfReplacement);
-    //    }
-
-
-    //    //1 and 2 and 3 check box
-    //    if (this.sourceWorkFlow.fixedAmount != undefined && this.sourceWorkFlow.fixedAmount != "" && this.sourceWorkFlow.percentOfNew != undefined && this.sourceWorkFlow.percentOfNew != "" && this.sourceWorkFlow.percentOfReplacement != undefined && this.sourceWorkFlow.percentOfReplacement != "") {
-    //        this.sourceWorkFlow.berThresholdAmount = Math.min(this.sourceWorkFlow.fixedAmount, this.sourceWorkFlow.percentOfNew, this.sourceWorkFlow.percentOfReplacement);
-    //    }
-    //    //1 and 2 and 3 check box all uncheck 
-    //    if (this.sourceWorkFlow.fixedAmount == undefined && this.sourceWorkFlow.percentOfNew == undefined && this.sourceWorkFlow.percentOfReplacement == undefined) {
-    //        this.sourceWorkFlow.berThresholdAmount = 0;
-    //    }
-    //}
-
-    
+   
 }
