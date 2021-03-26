0329a3a1043e5fc48adfec348e8380bf5c23e234
moin.silver@outlook.com <moin.silver@outlook.com>
2021-03-26 07:55:41 -0700

    LE Changes

--- src/app/components/general-ledger/entity/legal-entity-banking/legal-entity-banking.component.ts
+++ src/app/components/general-ledger/entity/legal-entity-banking/legal-entity-banking.component.ts
@@ -265,7 +265,7 @@ export class EntityBankingComponent implements OnInit, AfterViewInit {
 				setTimeout(() => {
 					this.CountryData(res[0].country)
 					this.glList();
-				}, 200)					
+				}, 500)					
 			}
 		}, err => {
 			this.isSpinnerVisible = false;
@@ -326,7 +326,7 @@ export class EntityBankingComponent implements OnInit, AfterViewInit {
 				}
 				setTimeout(() => {				
 					this.glList();
-				}, 200)	
+				}, 500)	
 			}
 		}, err => {
 			this.isSpinnerVisible = false;
@@ -359,7 +359,7 @@ export class EntityBankingComponent implements OnInit, AfterViewInit {
 				}
 				setTimeout(() => {				
 					this.glList();
-				}, 200)	
+				}, 500)	
 			}
 		}, err => {
 			this.isSpinnerVisible = false;
