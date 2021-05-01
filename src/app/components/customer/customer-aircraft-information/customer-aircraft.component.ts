57ea666c01f7b77f3c8d0fee2bfa2ac790a25593
Vamsi-Laptop <vamsi@silverxis.com>
2021-05-01 06:15:13 +0530

    Design Issues

--- src/app/components/customer/customer-aircraft-information/customer-aircraft.component.ts
+++ src/app/components/customer/customer-aircraft-information/customer-aircraft.component.ts
@@ -72,9 +72,9 @@ export class CustomerAircraftComponent implements OnInit {
     ];
     colsaircraftLD = [
         { field: "aircraftType", header: "Aircraft" },
-        { field: "aircraftModel", header: "Model",width:"60px" },
+        { field: "aircraftModel", header: "Model",width:"70px" },
         { field: "dashNumber", header: "Dash Numbers" },
-        { field: "inventory", header: "Inventory" ,width:"70px"},        
+        { field: "inventory", header: "Inventory" ,width:"90px"},        
         { field: 'createdDate', header: 'Created Date' },
         { field: 'createdBy', header: 'Created By' },     
         { field: 'updatedDate', header: 'Updated Date' },
