e82f102898d607ebd0f5940a4c0b2712e7642697
Vamsi-Laptop <vamsi@silverxis.com>
2021-05-12 17:12:06 +0530

    SO Print Form Changes

--- src/app/components/sales/order/sales-order-create/sales-order-create.component.ts
+++ src/app/components/sales/order/sales-order-create/sales-order-create.component.ts
@@ -1536,312 +1536,347 @@ export class SalesOrderCreateComponent implements OnInit {
     popupWin.document.write(`
       <html>
         <head>
-          <title>Sales Order</title>
+           <title>Sales Order</title>
           <style>
-          table {
-            width: 1000px;
-            overflow: auto !important;
-        }
-        
-        thead {
-          display: table-row-group;
-        }
-        
-        table thead {
-            background: #808080;
-            -webkit-print-color-adjust: exact;
-        }
-        
-        table thead tr {
-            background: #0d57b0 !important;
-            -webkit-print-color-adjust: exact;
-        }
-        
-        table,
-        thead,
-        td {
-            border: 1px solid black;
-            border-collapse: collapse;
-        }
-        
-        table,
-        thead,
-        th {
-            border: 1px solid black;
-            border-collapse: collapse;
-        }
-        
-        table thead tr th {
-            background: #0d57b0 !important;
-            padding: 5px !important;
-            color: #fff !important;
-            letter-spacing: 0.3px;
-            font-size: 10px;
-            text-transform: capitalize;
-            z-index: 1;
-            -webkit-print-color-adjust: exact;
-        }
-        
-        table tbody {
-            overflow-y: auto;
-            max-height: 500px;
-        }
-        
-        table tbody tr td {
-            background: #fff;
-            padding: 2px;
-            line-height: 22px;
-            height: 22px;
-            color: #333;
-            font-size: 11.5px !important;
-            letter-spacing: 0.1px;
-            -webkit-print-color-adjust: exact;
-        }
-        
-        h4 {
-            padding: 5px;
-            display: inline-block;
-            font-size: 14px;
-            font-weight: 600;
-            width: 100%;
-            margin: 0;
-        }
-        
-        h5 {
-            text-align: center;
-            background: #0d57b0 !important;
-            color: #fff !important;
-            margin-left: 48%;
-            -webkit-print-color-adjust: exact;
-        }
-        
-        hr {
-            margin-top: 10px;
-            margin-bottom: 10px;
-            border: 0;
-            border-top: 1px solid #e0e0e0;
-            height: 0;
-            box-sizing: content-box;
-            -webkit-print-color-adjust: exact;
-        }
-        
-        .first-block {
-            position: relative;
-            border: 1px solid black;
-            min-height: 1px;
-            float: left;
-            padding-right: 2px;
-            padding-left: 2px;
-            width: 66.66666667%;
-            -webkit-print-color-adjust: exact;
-        }
-        
-        .first-block-4 {
-            position: relative;
-            min-height: 1px;
-            float: left;
-            padding-right: 2px;
-            padding-left: 2px;
-        }
-        
-        .first-block-name {
-            margin-right: 20px
-        }
-        
-        .first-block-sold-to {
-            position: relative;
-            min-height: 200px;
-            float: left;
-            padding-right: 2px;
-            border: 1px solid black;
-            background: #fff;
-            width: 100%;
-            padding-left: 2px;
-            -webkit-print-color-adjust: exact;
-        }
-        
-        .first-block-ship-to {
-            position: relative;
-            min-height: 200px;
-            padding-right: 2px;
-            border: 1px solid black;
-            background: #fff;
-            width: 100%;
-            padding-left: 2px;
-            -webkit-print-color-adjust: exact;
-        }
-        
-        .first-block-sold {
-            position: relative;
-            min-height: 1px;
-            float: left;
-            padding-right: 2px;
-            padding-left: 2px;
-            width: 50%;
-            margin-top: 10px;
-        }
-        
-        .first-block-ship {
-            position: relative;
-            min-height: 1px;
-            float: right;
-            padding-right: 2px;
-            padding-left: 2px;
-            width: 48%;
-            margin-top: 10px
-        }
-        
-        .address-block {
-            position: relative;
-            min-height: 1px;
-            float: left;
-            padding-right: 2px;
-            border: 1px solid black;
-            width: 100%;
-            padding-left: 2px;
-            -webkit-print-color-adjust: exact;
-        }
-        
-        .first-block-address {
-            margin-right: 20px;
-            text-align: left
-        }
-        
-        .second-block {
-            position: relative;
-            min-height: 1px;
-            float: right;
-            padding-right: 2px;
-            width: 48%;
-            padding-left: 2px;
-            box-sizing: border-box;
-        }
-        
-        .second-block-div {
-            margin: 2px 0;
-            position: relative;
-            display: flex;
-            min-height: 1px;
-            padding-left: 0px;
-            width: 100%;
-        }
-        
-        .second-block-label {
-            position: relative;
-            min-height: 1px;
-            float: left;
-            padding-right: 2px;
-            padding-left: 2px;
-            width: 38.33333333%;
-            text-transform: capitalize;
-            margin-bottom: 0;
-            -webkit-print-color-adjust: exact;
-        }
-        
-        .clear {
-            clear: both;
-        }
-        
-        .form-div {
-            top: 6px;
-            position: relative;
-            font-weight: normal;
-            margin-top: 10px;
-        }
-        
-        .image {
-            border: 1px solid #ccc;
-            padding: 5px;
-            -webkit-print-color-adjust: exact;
-        }
-        
-        .logo-block {
-            margin: auto;
-            text-align: center
-        }
-        
-        .pdf-block {
-            width: 800px;
-            margin: auto;
-            border: 1px solid #ccc;
-            padding: 25px 15px;
-            -webkit-print-color-adjust: exact;
-        }
-        
-        .picked-by {
-            position: relative;
-            float: left;
-            width: 48%
-        }
-        
-        .confirmed-by {
-            position: relative;
-            float: right;
-            width: 48%
-        }
-        
-        .first-part {
-            position: relative;
-            display: flex;
-            float: left;
-            width: 50%
-        }
-        
-        .seond-part {
-            position: relative;
-            display: flex;
-            float: right;
-            width: 24%
-        }
-        
-        .input-field-border {
-            width: 88px;
-            border-radius: 0px !important;
-            border: none;
-            border-bottom: 1px solid black;
-        }
-        
-        .pick-ticket-header {
-            border: 1px solid black;
-            text-align: center;
-            background: #0d57b0 !important;
-            color: #fff !important;
-            -webkit-print-color-adjust: exact;
-        }
-        
-        .very-first-block {
-            position: relative;
-            min-height: 1px;
-            float: left;
-            padding-right: 2px;
-            padding-left: 2px;
-            width: 50%;
-        }
-        
-        .border-transparent {
-            border-block-color: white;
-            -webkit-print-color-adjust: exact;
-        }
+          // @page { size: auto;  margin: 0mm; }
 
-        .sales-order-header {
-          line-height: 25px;
-          margin-left: 42%;
-          -webkit-print-color-adjust: exact;
-        }
+              @media print
+	{
+		@page {
+		margin-top: 0;
+		margin-bottom: 0;
+		}
+	/*	body  {
+		padding-top: 72px;
+		padding-bottom: 72px ;
+		}*/
+     @page {size: landscape}
+	} 
+  span {
+    /* font-weight: normal; */
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+    font-size: 10.5px !important;
+    font-weight: 700;
+  }
+     
+  table {font-size:12px !important;
+    width: 100.6%;
+    overflow: auto !important;}        
+  table thead { background: #808080;}    
+   
+  table, thead, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+} 
+table, thead, th {
+  border: 1px solid black;
+  border-collapse: collapse;
+} 
+.border-none{
+  border:none;
+}
+  table thead tr th 
+  {
+    //   background: #0d57b0 !important;
+      padding: 5px!important;color: #fff;letter-spacing: 0.3px;font-weight:bold;
+      font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+       font-size: 12.5px;text-transform: capitalize; z-index: 1;} 
+  table tbody{   overflow-y: auto; max-height: 500px;  }
+  table tbody tr td{ background: #fff;
+     padding: 2px;line-height: 22px;
+     height:22px;color: #333;
+    //  border-right:1px solid black !important;
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+    font-size: 10.5px !important;
+    font-weight: 700;max-width:100%; letter-spacing: 0.1px;border:0}
 
-        .box-input {
-          color: #333 !important;
-          width: 62%;
-          text-align: left;
-          border: 1px solid #333;
-          border-radius: 2px;
-          height: 23px;
-          vertical-align: middle;
-          display: inline-block;
-          line-height: 20px;
-          margin-right: 0px !important;
-          -webkit-print-color-adjust: exact;
-        }
+  h4{padding: 5px; display: inline-block; font-size: 14px; font-weight: normal; width: 100%; margin: 0;}
+  
+  .very-first-block {position: relative; min-height: 1px;
+    height:auto;border-right:1px solid black;
+     float: left;padding-right: 2px;padding-left: 2px;
+    width: 50%;}
+  .first-block-name{margin-right: 20px} 
+  .first-block-sold-to {
+    position: relative;
+    min-height: 82px;
+    height: auto;
+    float: left;
+    padding-right: 2px;
+    border-right: 1px solid black;
+    background: #fff;
+    width: 100%;
+    margin-top:-2px
+   
+  }
+  
+  .first-block-ship-to {
+    position: relative;
+    min-height: 80px;
+    height: auto;
+    padding-right: 2px;
+    border-right: 1px solid black;
+    background: #fff;
+    width: 100%;
+  
+  }
+  
+  .first-block-sold {
+    position: relative;
+    min-height: 120px;
+    height:auto;
+    float: left;
+    border-right:1px solid black;
+    padding-right: 2px;
+    padding-left: 2px;
+    margin-left:-1px;
+    width: 50%;
+  }
+  
+  .first-block-ship {
+    position: relative;
+    min-height: 1px;
+    float: right;
+    padding-right: 2px;
+   
+    width: 48%;
+  }
+  
+  .address-block {
+    position: relative;
+    min-height: 119px;
+    float: left;
+    height:auto;
+
+    padding-right: 2px;
+    // border: 1px solid black;
+    width: 100%;
+    padding-left: 2px;
+  }
+  
+  .first-block-address {
+    margin-right: 20px;
+    text-align: left
+  }
+  
+  
+  .second-block {
+    position: relative;
+    min-height: 1px;
+    float: left;
+    padding-right: 2px;
+    width: 42%;
+  height:auto;
+    // border-left:1px solid black;
+      // margin-left: 16%;
+    padding-left: 2px;
+    box-sizing: border-box;
+  }
+  
+  .second-block-div {
+    margin: 2px 0;
+    position: relative;
+    display: flex;
+  
+    min-height: 1px;
+    height:auto
+   
+    width: 100%;
+  }
+  .label{
+    font-weight:500;
+  }
+  
+  .second-block-label {
+    position: relative;
+    min-height: 1px;
+    float: left;
+    padding-right: 2px;
+    padding-left: 2px;
+   
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+    font-size: 10.5px !important;
+    font-weight: 700;
+
+    width: 38.33333333%;
+    text-transform: capitalize;
+    margin-bottom: 0;
+    text-align: left;
+  }
+  
+  .clear {
+    clear: both;
+  }
+  
+  .form-div {
+    // top: 6px;
+    position: relative;
+    font-weight: normal;
+    font-size:12.5
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+    // margin-top: 10px;
+   
+  }
+  span {
+    font-weight: normal;
+    font-size: 12.5px !important;
+}
+  
+  .image {
+    border: 1px solid #000;
+    // padding: 5px;
+    width: 100%;
+    display: block;
+  }
+  
+  .logo-block {
+    margin: auto;
+    text-align: center
+  }
+  
+  .pdf-block {
+    width: 800px;
+    margin: auto;
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+    font-weight:normal;
+    border: 1px solid #ccc;
+    padding: 25px 15px;
+  }
+  
+  .picked-by {
+    position: relative;
+    float: left;
+    width: 48%;
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+    font-size: 10.5px !important;
+    font-weight: 700;
+  }
+  
+  .confirmed-by {
+    position: relative;
+    float: right;
+    width: 48%;font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+    font-size: 10.5px !important;
+    font-weight: 700;
+  }
+  
+  .first-part {
+    position: relative;
+    display: inline;
+    float: left;
+    width: 50%
+  }
+  
+  .seond-part {
+    position: relative;
+    display: flex;
+    float: right;
+    width: 24%
+  }
+  
+  .input-field-border {
+    width: 88px;
+    border-radius: 0px !important;
+    border: none;
+    border-bottom: 1px solid black;
+  }
+  
+  .border-transparent {
+    border-block-color: white;
+  }
+  
+  .pick-ticket-header {
+    border: 1px solid black;
+    text-align: center;
+    background: #0d57b0 !important;
+    color: #fff !important;
+    -webkit-print-color-adjust: exact;
+  }
+  
+  .first-block-label {
+    position: relative;
+    min-height: 1px;
+    float: left;
+    padding-right: 2px;
+    padding-left: 2px;
+    // width: 38.33333333%;
+    
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+    font-size: 10.5px !important;
+    font-weight: 700;
+
+    text-transform: capitalize;
+    margin-bottom: 0;
+    text-align: left;
+  }
+  
+  .very-first-block {
+    position: relative;
+    min-height: 1px;
+    height:auto;
+    border-right:1px solid black;
+    float: left;
+    padding-right: 2px;
+    padding-left: 2px;
+    width: 57% !important;
+  }
+  
+  .logo {
+     padding-top: 10px;
+    // height:70px;
+    // width:220px;
+    height:auto;
+    max-width:100%;
+    padding-bottom:10px;
+  
+  }
+  
+  .sold-block-div {
+    margin: 2px 0;
+    position: relative;
+    display: flex;
+    min-height: 1px;
+    width: 100%;
+  }
+  
+  .ship-block-div {
+    margin: 2px 0;
+    position: relative;
+    display: flex;
+    min-height: 1px;
+    width: 100%;
+  }
+  .first-block-sold-bottom{
+    border-bottom: 1px solid black;
+    position:relative;
+    min-height:1px;
+    height:auto;
+    width:100%;
+    float:left;
+      // margin-top: -2px;
+      height: auto;
+     // min-height: 120px;
+  }
+  
+  .parttable th {
+    background: #fff !important;
+    color: #000 !important;
+    -webkit-print-color-adjust: exact;
+  }
+  .border-bottom{
+    border-bottom:1px solid black !important;
+  }
+  .table-margins{
+    margin-top:0px;margin-left:-1px
+  }
+  .invoice-border{
+    border-bottom: 1px solid;
+    poeition:relative;
+      // min-height: 119px;
+      min-height:1px;
+      height: auto;
+    float:left;}
+ 
           </style>
         </head>
         <body onload="window.print();window.close()">${printContents}</body>
