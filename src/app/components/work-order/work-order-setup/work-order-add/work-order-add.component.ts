cb22dc0b769004b08bf760dc38018fabc348dea4
Hemant Patel <hemant_patel1234@yahoo.co.in>
2021-07-01 11:45:21 +0530

    Resolved Publish Error

--- src/app/components/work-order/work-order-setup/work-order-add/work-order-add.component.ts
+++ src/app/components/work-order/work-order-setup/work-order-add/work-order-add.component.ts
@@ -271,6 +271,7 @@ export class WorkOrderAddComponent implements OnInit {
     currentDate = new Date();
     taskName: any;
     isAllowLaberSave: boolean = false;
+
     constructor(
         private alertService: AlertService,
         private workOrderService: WorkOrderService,
@@ -3944,8 +3945,8 @@ export class WorkOrderAddComponent implements OnInit {
         this.currentWorkflowId = workOrderPartNumber.workflowId;
     }
     viewCmm(workOrderPartNumber){
-        this.currentcmmId=undefined;
-        this.currentcmmId=workOrderPartNumber.cMMId;
+        //this.currentcmmId=undefined;
+        //this.currentcmmId=workOrderPartNumber.cMMId;
     }     
     workflowTransfer:any={};
     currentRowIndex:any;
