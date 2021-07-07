f7d713eda66f6d6352289f77d6839d5c58427cae
Rajesh Gami <rmgami93@gmail.com>
2021-07-07 11:01:24 +0530

    Revert "When we give permission for Update OR View OR any other sub menu then it also selects "General Info" of Add permission also"
    This reverts commit 4e04d6e8f6c6724205aa51c548023f4e51ed53e7.

--- src/app/components/user-role/edit-user-roles.component.ts
+++ src/app/components/user-role/edit-user-roles.component.ts
@@ -321,6 +321,7 @@ export class EditUserRolesComponent implements OnInit {
 
     permissionChecked(event, currentModule: ModuleHierarchyMaster, type: string): void {
         var value = event.target.checked;   
+        this.enableDisableViewPermission(currentModule,value);  
             if (value == false) {
                 this.setPermissionByType(currentModule, type, value);
                 if (currentModule.parentId != null)
@@ -381,10 +382,12 @@ export class EditUserRolesComponent implements OnInit {
                 }
          });
        }
+
        this.sortedHierarchy.filter(function (module) {
                 if(genInfoCount >0){
                     if(module.parentId != null && (currentModule.parentId == module.parentId)){
                         if(module.displayOrder != null && module.displayOrder == 0 && value){
+                            module.rolePermission.canAdd = true;
                             module.rolePermission.canView = true;
                         }
                     }
